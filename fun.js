8a02f546b3924abfe25111b9ee7750ffa2a3df42
Freyayana <freyayana@gmail.com>
2018-10-07 23:26:46 +0300

    New files

---
+++ fun.js
@@ -0,0 +1,22 @@
+let x = 5;
+console.log(x);
+{
+    let x = 5;
+
+    x = 2;
+}
+console.log(x);
+console.log(4 < "10")
+console.log(0.1 + 0.2 == 0.3)
+console.log('abc' - 'bc')
+
+if ("false")
+{
+    console.log('Ваня алкаш!')
+}
+else
+{
+    console.log('Или нет 0_0')
+}
+1+(+"46")
+
